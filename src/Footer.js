fc2dff77ab2f5d7c491ef28c62807be995ca8685
Samuel <spage930@gmail.com>
2022-04-08 16:07:22 -0400

    Add NASA logo, footer credit, and year

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const Footer = () => {
+
+const date = new Date();
+const year = date.getFullYear();
+
   return (
     <footer className="footer">
-        <p></p>
+        <p> All information provided by nasa.gov {year}</p>
     </footer>
   )
 }
--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="header">
-      TESTING
-        <image alt="" src="/NAhttps://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svgSA-logo.svg" className="nasa-logo"></image>
+        <img alt="" src="/NASA_logo.png" className="nasa-logo"></img>
     </div>
   )
 }
